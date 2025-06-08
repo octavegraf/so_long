f199fba2746687303892be65e5cec412679a6d18
Octave Graf <ocgraf@student.42.fr>
2025-06-08 17:08:35 +0200

    reorganised

--- so_long.h
+++ so_long.h
@@ -6,16 +6,20 @@
 /*   By: ocgraf <ocgraf@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/03 14:25:58 by ocgraf            #+#    #+#             */
-/*   Updated: 2025/06/08 12:26:45 by ocgraf           ###   ########.fr       */
+/*   Updated: 2025/06/08 17:05:41 by ocgraf           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
-# include "mlx/mlx.h"
-# include "get_next_line/get_next_line.h"
 # include "ft_printf/ft_printf.h"
+# include "get_next_line/get_next_line.h"
+# include "libft/libft.h"
+# include "mlx/mlx.h"
 # include <errno.h>
+# include <string.h>
+
+# define ERR_INVALID_MAP 133
 
 #endif
