1ce0b983b495cc1886bf0f49c7e9f9474719889e
Octave Graf <cobra.morilles-0n@icloud.com>
2025-06-14 14:35:13 +0200

    updated macos keyboard + added functions

--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: ocgraf <ocgraf@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/03 14:25:58 by ocgraf            #+#    #+#             */
-/*   Updated: 2025/06/13 16:32:10 by ocgraf           ###   ########.fr       */
+/*   Updated: 2025/06/14 14:32:30 by ocgraf           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,16 @@
 # include <errno.h>
 # include <string.h>
 
-typedef struct s_data{
-	int		count;
+typedef struct s_data
+{
 	char	**map;
+	int		x;
+	int		y;
+	int		moves;
+	int		collectibles;
+	void	*mlx;
+	void	*win;
 }	t_data;
-
-typedef struct s_player_position {
-	int	x;
-	int	y;
-}	t_player_position;
 
 # ifdef __linux__
 #  define KEY_ESC 65307
@@ -41,27 +42,35 @@ typedef struct s_player_position {
 #  define KEY_RIGHT 65363
 # elif __APPLE__
 #  define KEY_ESC 53
-#  define KEY_W 13
-#  define KEY_S 1
-#  define KEY_A 0
-#  define KEY_D 2
-#  define KEY_UP 126
-#  define KEY_DOWN 125
-#  define KEY_LEFT 123
-#  define KEY_RIGHT 124
+#  define KEY_W 119
+#  define KEY_S 115
+#  define KEY_A 97
+#  define KEY_D 100
+#  define KEY_UP 65362
+#  define KEY_DOWN 65364
+#  define KEY_LEFT 65361
+#  define KEY_RIGHT 65363
 # endif
 
-char				*gnl_skip_whitespaces(int fd);
-int					check_map(char **map);
-int					check_map_collectibles(char	**map);
-int					different_char(char *str);
-int					file_checker(const char *path);
-char				**get_map(const char *path);
-int					check_map(char **map);
-char				**map_to_memory(const char *path);
-void				return_error(int error);
-t_player_position	get_position(char **map);
-int					move_up(t_data *data);
-int					move_down(t_data *data);
+void	get_position(t_data *data);
+int		move_up(t_data *data);
+int		move_down(t_data *data);
+int		move_left(t_data *data);
+int		move_right(t_data *data);
+int		collectibles_left(char **map);
+void	update_map(t_data *data);
+int		keyboard_events(int key, void *data, void *mlx, void *win);
+int		file_checker(const char *path);
+t_data	*so_long_init(char *map_path);
+char	*gnl_skip_whitespaces(int fd);
+int		different_char(char *str);
+int		check_map_components(char **map);
+int		check_map_chars(char	**map);
+int		check_map(char **map);
+char	**map_to_memory(const char *path);
+char	**get_map(const char *path);
+void	return_error(int error);
+void	free_it(char **this);
+void	leave(t_data *data);
 
 #endif
