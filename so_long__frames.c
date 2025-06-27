0e567038eaf874afcc98cc752f20b2e40d121307
Octave Graf <ocgraf@student.42.fr>
2025-06-27 14:25:36 +0200

    now checks if sprites are correctly initialized

--- so_long__frames.c
+++ so_long__frames.c
@@ -6,7 +6,7 @@
 /*   By: ocgraf <ocgraf@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/16 16:08:35 by ocgraf            #+#    #+#             */
-/*   Updated: 2025/06/26 16:55:05 by ocgraf           ###   ########.fr       */
+/*   Updated: 2025/06/27 12:05:44 by ocgraf           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,10 @@ void	init_sprites_frames_1(t_data *data)
 	int	height;
 
 	if ((!file_checker(BACKGROUND_1, data, 1))
-		|| (!file_checker(COLLECTIBLE_1, data, 1))
-		|| (!file_checker(EMPTY_1, data, 1))
-		|| (!file_checker(EXIT_1, data, 1))
-		|| (!file_checker(PLAYER_1, data, 1))
-		|| (!file_checker(WALL_1, data, 1)))
+		|| (!file_checker(COLLECTIBLE_1, data, 1)) || (!file_checker(EMPTY_1,
+				data, 1)) || (!file_checker(EXIT_1, data, 1))
+		|| (!file_checker(PLAYER_1, data, 1)) || (!file_checker(WALL_1,
+				data, 1)))
 		return ;
 	data->frame_1->background = mlx_xpm_file_to_image(data->mlx,
 			BACKGROUND_1, &width, &height);
@@ -37,6 +36,10 @@ void	init_sprites_frames_1(t_data *data)
 			PLAYER_1, &width, &height);
 	data->frame_1->wall = mlx_xpm_file_to_image(data->mlx,
 			WALL_1, &width, &height);
+	if (!data->frame_1->background || !data->frame_1->collectible
+		|| !data->frame_1->empty || !data->frame_1->exit
+		|| !data->frame_1->player || !data->frame_1->wall)
+		return_error(134, data);
 }
 
 void	init_sprites_frames_2(t_data *data)
@@ -44,12 +47,11 @@ void	init_sprites_frames_2(t_data *data)
 	int	width;
 	int	height;
 
-	if ((!file_checker(BACKGROUND_2, data, 1))
-		|| (!file_checker(COLLECTIBLE_2, data, 1))
-		|| (!file_checker(EMPTY_2, data, 1))
-		|| (!file_checker(EXIT_2, data, 1))
-		|| (!file_checker(PLAYER_2, data, 1))
-		|| (!file_checker(WALL_2, data, 1)))
+	if ((!file_checker(BACKGROUND_2, data, 2))
+		|| (!file_checker(COLLECTIBLE_2, data, 2)) || (!file_checker(EMPTY_2,
+				data, 2)) || (!file_checker(EXIT_2, data, 2))
+		|| (!file_checker(PLAYER_2, data, 2)) || (!file_checker(WALL_2,
+				data, 2)))
 		return ;
 	data->frame_2->background = mlx_xpm_file_to_image(data->mlx,
 			BACKGROUND_2, &width, &height);
@@ -63,6 +65,10 @@ void	init_sprites_frames_2(t_data *data)
 			PLAYER_2, &width, &height);
 	data->frame_2->wall = mlx_xpm_file_to_image(data->mlx,
 			WALL_2, &width, &height);
+	if (!data->frame_2->background || !data->frame_2->collectible
+		|| !data->frame_2->empty || !data->frame_2->exit
+		|| !data->frame_2->player || !data->frame_2->wall)
+		return_error(134, data);
 }
 
 int	frames(t_data *data)
